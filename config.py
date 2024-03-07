081365c4ff2cd354000e955ca22041e9385a1251
Hritik086 <151951067+Hritik086@users.noreply.github.com>
2024-03-07 11:58:49 +0530

    Update config.py
    Signed-off-by: Hritik086 <151951067+Hritik086@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://telegra.ph/file/cd43274935c549ef20beb.jpg",
+    "https://github.com/Hritik086/amtra5",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
