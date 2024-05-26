8c8f52a57ef7c4b5513344d1acebfbb8bc96f2c8
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-27 01:40:39 +0530

    Update start.py
    Signed-off-by: RAJNISH XD <155803358+doraemon890@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -1,37 +1,55 @@
 from pyrogram.types import InlineKeyboardButton
-
 import config
 from AnonXMusic import app
 
-
-def start_panel(_):
+def start_panel(localized_text):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=localized_text["S_B_1"], 
+                url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text=localized_text["S_B_2"], 
+                url=config.SUPPORT_CHAT
+            ),
         ],
     ]
     return buttons
 
-
-def private_panel(_):
+def private_panel(localized_text):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+                text=localized_text["S_B_3"], 
+                url=f"https://t.me/{app.username}?startgroup=true"
+            ),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url="https://telegra.ph/file/c45acbf0c5107a79952c7.mp4)",
+            InlineKeyboardButton(
+                text=localized_text["S_B_4"], 
+                callback_data="settings_back_helper"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=localized_text["S_B_5"], 
+                user_id=config.OWNER_ID
+            ),
+            InlineKeyboardButton(
+                text=localized_text["S_B_2"], 
+                url=config.SUPPORT_CHAT
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=localized_text["S_B_6"], 
+                url=config.SUPPORT_CHANNEL
+            ),
+            InlineKeyboardButton(
+                text=localized_text["S_B_7"], 
+                url="https://telegra.ph/file/c45acbf0c5107a79952c7.mp4"
+            ),
         ],
     ]
     return buttons
